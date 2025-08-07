e6701af0851400858e04c2ece8c3065235cb2f97
Jure Perak <jure.perak@moberg.hr>
2025-08-07 09:54:51 +0200

    Removed .cjs as this will never be used on backend.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  injectStyle: true,
+  format: ["esm", "iife"], // ESM for modern browsers, IIFE for immediate execution
+  globalName: "StraumurCheckout", // for IIFE: window.StraumurCheckout
+  dts: true, // this allows other TypeScript projects (like merchant apps) to get type safety and autocompletion when they import your package
+  injectStyle: true, // this option allows you to inject CSS styles directly into the output bundle, which is useful for web components that need styles
   clean: true, // is an option in tsup that tells it to delete the output directory (like dist/) before each new build.
-  sourcemap: true, // A sourcemap is a file (or embedded info) that maps your final bundled/minified code back to your original source code
+  sourcemap: true, // a sourcemap is a file (or embedded info) that maps your final bundled/minified code back to your original source code
   bundle: true, // bundles all dependencies into the output files
   external: [], // specify external dependencies that should not be bundled
   splitting: false, // disable code-splitting for easier CDN usage
@@ -15,8 +16,8 @@ export default defineConfig({
     ".svg": "jsx",
   },
   outExtension({ format }) {
-    return {
-      js: format === "esm" ? ".mjs" : ".cjs",
-    };
+    if (format === "esm") return { js: ".mjs" };
+    if (format === "iife") return { js: ".js" };
+    return { js: ".js" };
   },
 });
