595a23ac8fbe75907e74b24c6e933e6a27d2064d
Jure Perak <jure.perak@moberg.hr>
2025-11-20 17:04:22 +0100

    use entire space when single payment wallet

--- src/features/instantPayments/instant-payments-component.tsx
+++ src/features/instantPayments/instant-payments-component.tsx
@@ -4,6 +4,7 @@ import { StraumurCheckoutConfiguration } from "../../models/models";
 import { SuccessResponse } from "../../services/models";
 import GooglePayButton from "../../components/google-pay-button/google-pay-button";
 import ApplePayButton from "../../components/apple-pay-button/apple-pay-button";
+import { PaymentMethod } from "../../models/constants";
 
 interface InstantPaymentsComponentProps {
   configuration: StraumurCheckoutConfiguration;
@@ -18,14 +19,47 @@ function InstantPaymentsComponent({
     return null;
   }
 
+  // safeguard: filter out any invalid payment methods, only allow applepay and googlepay
+  const validInstantPayments: Extract<PaymentMethod, "googlepay" | "applepay">[] = ["googlepay", "applepay"];
+  const availableInstantPayments = configuration.instantPayments.filter((payment) =>
+    validInstantPayments.includes(payment)
+  );
+
+  if (availableInstantPayments.length === 0) {
+    return null;
+  }
+
   return (
-    <div class="instant-payments">
-      {configuration.instantPayments.some((x) => x === "googlepay") && (
-        <GooglePayButton configuration={configuration} paymentMethods={paymentMethods} showPaymentButton={true} />
-      )}
-      {configuration.instantPayments.some((x) => x === "applepay") && (
-        <ApplePayButton configuration={configuration} paymentMethods={paymentMethods} showPaymentButton={true} />
-      )}
+    <div
+      class={`instant-payments ${
+        availableInstantPayments.length > 1 ? "instant-payments--multiple" : "instant-payments--single"
+      }`}
+    >
+      {availableInstantPayments.map((paymentMethod) => {
+        if (paymentMethod === "googlepay") {
+          return (
+            <GooglePayButton
+              key={paymentMethod}
+              configuration={configuration}
+              paymentMethods={paymentMethods}
+              showPaymentButton={true}
+            />
+          );
+        }
+        if (paymentMethod === "applepay") {
+          return (
+            <ApplePayButton
+              key={paymentMethod}
+              configuration={configuration}
+              paymentMethods={paymentMethods}
+              showPaymentButton={true}
+            />
+          );
+        }
+
+        // this should never happen due to our filtering above, but typescript safeguard
+        return null;
+      })}
     </div>
   );
 }
